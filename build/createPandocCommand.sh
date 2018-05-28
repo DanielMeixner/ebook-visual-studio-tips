2999bf7fd324138e392a720c285e46d34bd0eded
DanielMeixner <daniel.meixner@live.com>
2018-05-28 17:22:55 +0200

    md doc

--- build/createPandocCommand.sh
+++ build/createPandocCommand.sh
@@ -1,7 +1,7 @@
 echo "/bin/pandoc -o VisualStudioTipps.epub title.txt \ " >> ebookbuilder.sh
-for d in `find ../content -type d`
+for doc in `find ../content -name *.md`
 do
-  echo $d/tipp.md \ >> ebookbuilder.sh
+  echo $doc \ >> ebookbuilder.sh
 done
 echo --epub-cover-image ../content/coverimage.png --number-sections >> ebookbuilder.sh
 
