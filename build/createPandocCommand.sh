1874e169ee507a0ee1f40933737a2de9638771d8
DanielMeixner <daniel.meixner@live.com>
2018-05-28 16:43:44 +0200

    missing leading /

--- build/createPandocCommand.sh
+++ build/createPandocCommand.sh
@@ -1,6 +1,6 @@
-echo "bin/pandoc -o guide.epub title.txt \ " >> ebookbuilder.sh
+echo "/bin/pandoc -o guide.epub title.txt \ " >> ebookbuilder.sh
 for d in 'find ../content -type d'
 do
   echo $d/tipp.md \ >> ebookbuilder.sh
 done
-echo --epub-cover-image coverimage.jpg --number-sections >> ebookbuilder.sh
+echo --epub-cover-image ../content/coverimage.jpg --number-sections >> ebookbuilder.sh
