bb573fb88a282d5cc5be93c154719d487caef5b8
DanielMeixner <daniel.meixner@live.com>
2018-05-29 09:27:59 +0200

    ebookbuilder script in build folder

--- build/createPandocCommand.sh
+++ build/createPandocCommand.sh
@@ -1,10 +1,10 @@
-echo "/bin/pandoc -o VisualStudioTipps.epub build/title.txt \ " >> ebookbuilder.sh
+echo "/bin/pandoc -o VisualStudioTipps.epub build/title.txt \ " >> build/ebookbuilder.sh
 for doc in `find . -name "*.md" | sed "s|^\./||"`
 do
 
-  echo " $doc \ "  >> ebookbuilder.sh
+  echo " $doc \ "  >> build/ebookbuilder.sh
 done
-echo " --epub-cover-image coverimage.png --number-sections" >> ebookbuilder.sh
+echo " --epub-cover-image coverimage.png --number-sections" >> build/ebookbuilder.sh
 
-echo created file:
-echo $(cat ebookbuilder.sh)
+echo created file to build ebook:
+echo $(cat build/ebookbuilder.sh)
