1a05dfa44f66e684d83ff2d3abeaba007133b7b1
DanielMeixner <daniel.meixner@live.com>
2018-05-29 10:25:59 +0200

    removed "

--- build/createPandocCommand.sh
+++ build/createPandocCommand.sh
@@ -1,8 +1,8 @@
-echo "bin/pandoc -o VisualStudioTipps.epub build/title.txt \ " >> build/ebookbuilder.sh
+echo -n "bin/pandoc -o VisualStudioTipps.epub build/title.txt " >> build/ebookbuilder.sh
 for doc in `find . -name "*.md" | sed "s|^\./||"`
 do
 
-  echo -n " $doc \ "  >> build/ebookbuilder.sh
+  echo -n " $doc "  >> build/ebookbuilder.sh
 done
 echo -n " --epub-cover-image content/coverimage.png --number-sections" >> build/ebookbuilder.sh
 
