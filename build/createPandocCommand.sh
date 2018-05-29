6f368c5d47d42cb5520542e1f6f92b165bcce75b
DanielMeixner <daniel.meixner@live.com>
2018-05-29 11:08:58 +0200

    added .. in front of path due to work dir change

--- build/createPandocCommand.sh
+++ build/createPandocCommand.sh
@@ -1,10 +1,10 @@
-echo -n "bin/pandoc -o VisualStudioTipps.epub ../build/title.txt " >> build/ebookbuilder.sh
+echo -n "bin/pandoc -o VisualStudioTipps.epub ../build/title.txt " >> ../build/ebookbuilder.sh
 for doc in `find . -name "*.md" | sed "s|^\./||"`
 do
 
-  echo -n " $doc "  >> build/ebookbuilder.sh
+  echo -n " $doc "  >> ../build/ebookbuilder.sh
 done
-echo -n " --epub-cover-image coverimage.png --number-sections" >> build/ebookbuilder.sh
+echo -n " --epub-cover-image coverimage.png --number-sections" >> ../build/ebookbuilder.sh
 
 echo created file to build ebook:
-echo $(cat build/ebookbuilder.sh)
+echo $(cat ../build/ebookbuilder.sh)
