471bb3b47c80cae2a3b46f8fe963165520e1e4fc
DanielMeixner <daniel.meixner@live.com>
2018-06-25 13:22:38 +0200

    added quotes for filename with spaces

--- build/createPandocCommand.sh
+++ build/createPandocCommand.sh
@@ -2,7 +2,7 @@ echo -n "bin/pandoc -o VisualStudioTipps.epub ../build/title.txt " >> ../build/e
 for doc in `find . -name "*.md" | sort -g  | sed "s|^\./||"`
 do
 
-  echo -n " $doc "  >> ../build/ebookbuilder.sh
+  echo -n " \"$doc\" "  >> ../build/ebookbuilder.sh
 done
 echo -n " --epub-cover-image coverimage.png --number-sections" >> ../build/ebookbuilder.sh
 
