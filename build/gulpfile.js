adbae5a97c41dd714552ebcee459ba069bec4112
DanielMeixner <daniel.meixner@live.com>
2018-06-25 12:02:55 +0200

    gulp filter and sort

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -2,11 +2,17 @@ var gulp = require('gulp');
 var toc = require('gulp-markdown-toc');
 
 gulp.task('toc', function () {
-  return gulp.src('../content/*.md')
+  return gulp.src('../content/')
     .pipe(toc(
       {
+        filter: function (file) {
+          return file.relative == '*.md'
+        },
+        sort: function (fileA, fileB) {
+          return fileA.relative < fileB.relative;
+        },
         index: function (file) {
-          file.basename = '00_Index/index.md'; return 'done';
+          file.basename = 'index.md'; return '00_Index';
         }
       }
     ))
